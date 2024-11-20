0c5a3acee645e8980a51cefe0201696eda119b2e
Tavish108 <sharmatavish318@gmail.com>
2024-11-20 17:34:13 +0530

    jobquester

---
+++ app/interview/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <iframe
+        src="/webrtc/mumble2/lobby.html"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+      ></iframe>
+    </div>
+  );
+}
+
+export default page
